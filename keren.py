ca406ecff4a9f2609d3acfe6481f4f7ed5d2cb41
Briant242 <rianjbasli@gmail.com>
2026-06-01 09:32:12 +0700

    Penerapan sederhana input, print, dan juga f-string

--- keren.py
+++ keren.py
@@ -1,2 +1,8 @@
-sapaan = 'Anjay kamu siapa?'
-print(sapaan)
+import time
+
+greeting = 'Hello, how are you?'
+
+user_name = input('Please enter your name: ')
+print(greeting)
+time.sleep(1)
+print(f'Nice to meet you, {user_name}!')
