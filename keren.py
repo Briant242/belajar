a5e9c0216791e0cb224f07e704208bc2a260ae2a
Briant242 <rianjbasli@gmail.com>
2026-06-01 11:02:45 +0700

    Structure Fixing

--- keren.py
+++ keren.py
@@ -3,6 +3,6 @@ import time
 greeting = 'Hello, how are you?'
 
 user_name = input('Please enter your name: ')
-print(greeting)
-time.sleep(1)
 print(f'Nice to meet you, {user_name}!')
+time.sleep(1)
+print(greeting)
